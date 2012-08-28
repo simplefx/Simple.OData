5ea0f2e32e4fca613122d8938ce1837e9baa4db0
Vagif Abilov <vagif@online.no>
2012-08-28 17:01:52 +0200

    Updated version number to 0.4.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.3.1.0")]
-[assembly: AssemblyFileVersion("0.3.1.0")]
-[assembly: AssemblyInformationalVersion("0.3.1.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyInformationalVersion("0.4.0.0")]
