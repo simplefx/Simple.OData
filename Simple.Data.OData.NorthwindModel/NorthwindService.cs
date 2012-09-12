b40a5f834154f7af5a61f8af1cae6cdea875e3fa
Vagif Abilov <vagif@online.no>
2012-09-12 22:40:18 +0200

    Replaced in-memory with SQL CE database.

--- Simple.Data.OData.NorthwindModel/NorthwindService.cs
+++ Simple.Data.OData.NorthwindModel/NorthwindService.cs
@@ -1,26 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Data.Services;
 using System.Data.Services.Common;
 using System.Linq;
 using System.Text;
-using Simple.Data.OData.NorthwindModel;
 
 namespace Simple.Data.OData.NorthwindModel
 {
-    public class NorthwindService : DataService<NorthwindContext>
+    public class NorthwindService : DataService<NorthwindEntities>
     {
         public static void InitializeService(DataServiceConfiguration config)
         {
+            Database.DefaultConnectionFactory = new SqlCeConnectionFactory("System.Data.SqlServerCe.4.0");
+
             config.SetEntitySetAccessRule("*", EntitySetRights.All);
             config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
             config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
             config.UseVerboseErrors = true;
-        }
-
-        protected override NorthwindContext CreateDataSource()
-        {
-            return NorthwindContext.Instance;
         }
     }
 }
