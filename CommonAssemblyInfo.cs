2de44ee09c70ac258dfb7549a5275cc40d0b6985
Vagif Abilov <vagif@online.no>
2012-12-15 23:09:55 +0100

    Version 0.8.1.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.8.0.1")]
-[assembly: AssemblyFileVersion("0.8.0.1")]
-[assembly: AssemblyInformationalVersion("0.8.0.1")]
+[assembly: AssemblyVersion("0.8.1.0")]
+[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyInformationalVersion("0.8.1.0")]
