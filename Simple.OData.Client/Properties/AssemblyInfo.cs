968d77b9f6a33b7556ae99eaea05662268ab35bd
Vagif Abilov <vagif.abilov@gmail.com>
2012-12-28 15:59:43 +0100

    Corrected assembly names.

--- Simple.OData.Client/Properties/AssemblyInfo.cs
+++ Simple.OData.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Simple.OData")]
+[assembly: AssemblyTitle("Simple.OData.Client")]
 [assembly: AssemblyDescription("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c39637fa-a4e4-4ca4-a0a8-f393f35e9a72")]
 
 [assembly: InternalsVisibleTo("Simple.OData.Client.Tests")]
+[assembly: InternalsVisibleTo("Simple.OData.Client.Tests.Net40")]
 [assembly: InternalsVisibleTo("Simple.OData.Client.Tests.WinRT")]
