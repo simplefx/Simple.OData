a71d80fb92afc4a09a1a0a99bbd6f19587ca02dd
Vagif Abilov <vagif.abilov@gmail.com>
2014-02-04 15:52:37 +0100

    Added Insert/Update/Test for Web API.

--- Simple.OData.Client.Tests.Net40/WebApiTests.cs
+++ Simple.OData.Client.Tests.Net40/WebApiTests.cs
@@ -1,22 +1,96 @@
 ﻿using System;
 using System.Linq;
-using Simple.OData.Client.TestUtils;
 using Xunit;
+
+using Entry = System.Collections.Generic.Dictionary<string, object>;
 
 namespace Simple.OData.Client.Tests
 {
-    public class WebApiTests : TestBase
+    public class WebApiTests : IDisposable
     {
+        protected string _serviceUri;
+        protected IODataClient _client;
+
+        public WebApiTests()
+        {
+            _client = new ODataClient("http://localhost:51849/odata");
+        }
+
+        public void Dispose()
+        {
+            if (_client != null)
+            {
+                var products = _client.FindEntries("Products");
+                foreach (var product in products)
+                {
+                    if (product["Name"].ToString().StartsWith("Test"))
+                        _client.DeleteEntry("Products", product);
+                }
+            }
+        }
+
         [Fact]
         public void GetProductsCount()
         {
-            var client = new ODataClient("http://localhost:51849/odata");
-
-            var products = client
+            var products = _client
                 .For("Products")
                 .FindEntries();
 
             Assert.Equal(5, products.Count());
+        }
+
+        [Fact]
+        public void InsertProduct()
+        {
+            var product = _client
+                .For("Products")
+                .Set(new Entry() { { "Name", "Test1" }, { "Price", 18m } })
+                .InsertEntry();
+
+            Assert.Equal("Test1", product["Name"]);
+        }
+
+        [Fact]
+        public void UpdateProduct()
+        {
+            var product = _client
+                .For("Products")
+                .Set(new { Name = "Test1", Price = 18m })
+                .InsertEntry();
+
+            _client
+                .For("Products")
+                .Key(product["ID"])
+                .Set(new { Price = 123m })
+                .UpdateEntry();
+
+            product = _client
+                .For("Products")
+                .Filter("Name eq 'Test1'")
+                .FindEntry();
+
+            Assert.Equal(123m, product["Price"]);
+        }
+
+        [Fact]
+        public void DeleteProduct()
+        {
+            var product = _client
+                .For("Products")
+                .Set(new { Name = "Test1", Price = 18m })
+                .InsertEntry();
+
+            _client
+                .For("Products")
+                .Key(product["ID"])
+                .DeleteEntry();
+
+            product = _client
+                .For("Products")
+                .Filter("Name eq 'Test1'")
+                .FindEntry();
+
+            Assert.Null(product);
         }
     }
 }
