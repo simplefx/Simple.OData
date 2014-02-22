9cba0f186200862caed96149f1874603127f1e73
Vagif Abilov <vagif.abilov@gmail.com>
2014-02-22 23:42:17 +0100

    Added references to Microsoft.Bcl.Build and Async to test projects.

--- CommonAssemblyVersion.cs
+++ CommonAssemblyVersion.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: AssemblyInformationalVersion("1.3.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
