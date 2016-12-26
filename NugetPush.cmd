607381b73d5e6a1f9afc454cabb168502bf83e52
Vagif Abilov <vagif.abilov@gmail.com>
2016-12-26 20:39:25 +0100

    Correction in Nuget push script.

--- NugetPush.cmd
+++ NugetPush.cmd
@@ -1,3 +1,3 @@
-call nuget push Packages\Simple.OData.Client.%1.nupkg %2 -Source nuget.org 
-call nuget push Packages\Simple.OData.V3.Client.%1.nupkg %2 -Source nuget.org 
-call nuget push Packages\Simple.OData.V4.Client.%1.nupkg %2 -Source nuget.org 
+call nuget push Packages\Simple.OData.Client.%1.nupkg %2 -Source https://nuget.org 
+call nuget push Packages\Simple.OData.V3.Client.%1.nupkg %2 -Source https://nuget.org 
+call nuget push Packages\Simple.OData.V4.Client.%1.nupkg %2 -Source https://nuget.org 
