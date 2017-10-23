2ad11a00f55e3161ee31b7ec396267badffd9450
Vagif Abilov <vagif.abilov@gmail.com>
2017-10-23 16:45:17 +0200

    Fixed acceptance tests.

---
+++ Simple.OData.Any.Client.Tests/Utils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Simple.OData.Client
+{
+    static class Utils
+    {
+        public static string StreamToString(Stream stream, bool disposeStream = false)
+        {
+            if (!disposeStream && stream.CanSeek)
+                stream.Seek(0, SeekOrigin.Begin);
+            var result = new StreamReader(stream).ReadToEnd();
+            if (disposeStream)
+                stream.Dispose();
+            return result;
+        }
+
+        public static byte[] StreamToByteArray(Stream stream, bool disposeStream = false)
+        {
+            if (!disposeStream && stream.CanSeek)
+                stream.Seek(0, SeekOrigin.Begin);
+            var bytes = new byte[stream.Length];
+            var result = new BinaryReader(stream).ReadBytes(bytes.Length);
+            if (disposeStream)
+                stream.Dispose();
+            return result;
+        }
+
+        public static Stream StringToStream(string text)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(text));
+        }
+
+        public static Stream ByteArrayToStream(byte[] bytes)
+        {
+            return new MemoryStream(bytes);
+        }
+
+        public static Stream CloneStream(Stream stream)
+        {
+            stream.Position = 0;
+            var clonedStream = new MemoryStream();
+            stream.CopyTo(clonedStream);
+            return clonedStream;
+        }
+    }
+}
---
+++ Simple.OData.V4.Client.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Simple.OData.V4.Client.Tests")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d48a0a7-1305-44a8-bdc4-49cadebf0874")]
