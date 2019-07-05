22b8316ebe48c822e052bf632cbea1f252431885
Michael Sollenberger <michael.sollenberger@avanade.com>
2019-07-05 15:14:57 -0400

    Added test for RequestWriter.

---
+++ src/Simple.OData.Client.UnitTests/Core/RequestWriterActionTests.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Simple.OData.Client.Tests.Core
+{
+
+    public class RequestWriterActionTests : CoreTestBase
+    {
+        public override string MetadataFile => "ActionWithDerivedType.xml";
+        public override IFormatSettings FormatSettings => new ODataV4Format();
+
+        protected async Task<IRequestWriter> CreateRequestWriter()
+        {
+            return new V4.Adapter.RequestWriter(_session, await _client.GetMetadataAsync<Microsoft.OData.Edm.IEdmModel>(), null);
+        }
+
+        [Fact]
+        public async Task CreateActionRequestWithDerivedType()
+        {
+            var requestWriter = await CreateRequestWriter();
+            var request = await requestWriter.CreateActionRequestAsync("CancelSalesOrder", "CancelSalesOrder", null,
+                new Dictionary<string, object>
+                {
+                    ["Status"] = 936140011,
+                    ["OrderClose"] = new Dictionary<string, object>
+                    {
+                        ["salesorderid"] = new Dictionary<string, object> { { "salesorderid", new Guid("5AEEC23F-DB4F-E911-A978-000D3A3611E1") } },
+                        ["subject"] = $"Close sales Order {DateTime.Now}",
+                        ["@odata.type"] = "Microsoft.Dynamics.CRM.orderclose"
+                    }
+                }, true);
+            var stringResult = await request.RequestMessage.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<JObject>(stringResult);
+
+            Assert.True(result.ContainsKey("OrderClose"));
+            
+            var orderClose = result["OrderClose"];
+
+            Assert.NotNull(orderClose);
+            Assert.NotNull(orderClose["@odata.type"]);
+            Assert.Equal("#Microsoft.Dynamics.CRM.orderclose", ((JValue)orderClose["@odata.type"]).Value as string);
+        }
+    }
+}
