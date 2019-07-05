420e9af7b04f267b16b937a9b43b827d1c7df545
Paul Kröner <paul.kroener@l-mobile.com>
2019-07-05 11:26:05 +0200

    added test using custom adapter factory

---
+++ src/Simple.OData.Client.UnitTests/Core/AdapterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace Simple.OData.Client.Tests.Core
+{
+    public class AdapterTests : TestBase
+    {
+        class CustomAdapter : V4.Adapter.ODataAdapter
+        {
+            public CustomAdapter(ISession session, IODataModelAdapter modelAdapter) : base(session, modelAdapter)
+            {
+            }
+        }
+        class CustomAdapterFactory : AdapterFactory
+        {
+            public override Func<ISession, IODataAdapter> CreateAdapterLoader(string metadataString, ITypeCache typeCache)
+            {
+                var modelAdapter = CreateModelAdapter(metadataString, typeCache);
+                return session => new CustomAdapter(session, modelAdapter);
+            }
+        }
+        [Fact]
+        public void UseCustomAdapter()
+        {
+            var settings = CreateDefaultSettings();
+            settings.AdapterFactory = new CustomAdapterFactory();
+            var client = new ODataClient(settings);
+            Assert.IsType<CustomAdapter>(client.Session.Adapter);
+        }
+    }
+}
