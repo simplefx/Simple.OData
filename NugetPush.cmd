19024c389962e177546117ca6e7e127b0842405e
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2019-10-03 12:12:52 +0300

    Upgraded symbol publishing

--- NugetPush.cmd
+++ NugetPush.cmd
@@ -1,3 +1,3 @@
-call nuget push build\packages\Simple.OData.Client.%1.nupkg %2 -Source https://nuget.org 
+call nuget push build\packages\Simple.OData.Client.%1.nupkg    %2 -Source https://nuget.org 
 call nuget push build\packages\Simple.OData.V3.Client.%1.nupkg %2 -Source https://nuget.org 
 call nuget push build\packages\Simple.OData.V4.Client.%1.nupkg %2 -Source https://nuget.org 
