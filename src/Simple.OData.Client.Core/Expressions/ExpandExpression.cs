a4eeb3b462b3ce3e77ec4ee73985a69c39971dcd
maxbruecken <becker.maxim@gmail.com>
2020-08-24 20:25:21 +0200

    replaced recursion/lazy iteration by expression visitor

--- src/Simple.OData.Client.Core/Expressions/ExpandExpression.cs
+++ src/Simple.OData.Client.Core/Expressions/ExpandExpression.cs
@@ -7,120 +7,117 @@ namespace Simple.OData.Client
 {
     internal static class ExpandExpression
     {
-        public static IEnumerable<ODataExpandAssociation> ExtractExpandAssociations<T>(this Expression<Func<T, object>> expression,
+        public static IEnumerable<ODataExpandAssociation> ExtractExpandAssociations<T>(
+            this Expression<Func<T, object>> expression,
             ITypeCache typeCache)
         {
-            var lambdaExpression = expression as LambdaExpression;
-            if (lambdaExpression == null)
-                throw Utils.NotSupportedExpression(expression);
+            var expandExpressionVisitor = new ExpandExpressionVisitor(typeCache);
+            expandExpressionVisitor.Visit(expression);
 
-            switch (lambdaExpression.Body.NodeType)
+            if (expandExpressionVisitor.ExpandAssociations.Any())
+                return expandExpressionVisitor.ExpandAssociations;
+
+            throw Utils.NotSupportedExpression(expression);
+        }
+    }
+
+    internal class ExpandExpressionVisitor : ExpressionVisitor
+    {
+        private readonly ITypeCache _typeCache;
+        
+        public ExpandExpressionVisitor(ITypeCache typeCache)
+        {
+            _typeCache = typeCache;
+        }
+
+        public List<ODataExpandAssociation> ExpandAssociations { get; } = new List<ODataExpandAssociation>();
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            var memberName = _typeCache.GetMappedName(node.Expression.Type, node.Member.Name);
+
+            ExpandAssociations.AddRange(node.Expression is MemberExpression
+                ? ExtractNestedExpandAssociations(node.Expression)
+                : new[] { new ODataExpandAssociation(memberName) });
+            
+            return node;
+        }
+
+        protected override Expression VisitUnary(UnaryExpression node)
+        {
+            if (node.NodeType != ExpressionType.Convert) return base.VisitUnary(node);
+            
+            ExpandAssociations.AddRange(ExtractNestedExpandAssociations(node.Operand));
+            return node;
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Arguments.Count != 2)
+                throw Utils.NotSupportedExpression(node);
+
+            var association = ExtractNestedExpandAssociations(node.Arguments[0]).Single();
+            ExpandAssociations.Add(association);
+
+            switch (node.Method.Name)
             {
-                case ExpressionType.MemberAccess:
-                case ExpressionType.Convert:
-                    return ExtractExpandAssociations(lambdaExpression.Body, typeCache);
+                case "Select":
+                {
+                    association.ExpandAssociations.AddRange(ExtractNestedExpandAssociations(node.Arguments[1]));
 
-                case ExpressionType.Call:
-                    return ExtractExpandAssociations(lambdaExpression.Body as MethodCallExpression, typeCache);
+                    return node;
+                }
+                case "OrderBy":
+                case "ThenBy":
+                {
+                    if ((node.Arguments[0] as MethodCallExpression)?.Method.Name == "Select")
+                        throw Utils.NotSupportedExpression(node);
 
-                case ExpressionType.New:
-                    return ExtractExpandAssociations(lambdaExpression.Body as NewExpression, typeCache);
+                    association.OrderByColumns
+                        .AddRange(node.Arguments[1]
+                            .ExtractColumnNames(_typeCache).Select(c => new ODataOrderByColumn(c, false)));
 
+                    return node;
+                }
+                case "OrderByDescending":
+                case "ThenByDescending":
+                {
+                    if ((node.Arguments[0] as MethodCallExpression)?.Method.Name == "Select")
+                        throw Utils.NotSupportedExpression(node);
+
+                    association.OrderByColumns
+                        .AddRange(node.Arguments[1]
+                            .ExtractColumnNames(_typeCache).Select(c => new ODataOrderByColumn(c, true)));
+
+                    return node;
+                }
+                case "Where":
+                {
+                    var filterExpression =
+                        ODataExpression.FromLinqExpression((node.Arguments[1] as LambdaExpression)?.Body);
+                    if (ReferenceEquals(association.FilterExpression, null))
+                        association.FilterExpression = filterExpression;
+                    else
+                        association.FilterExpression = association.FilterExpression && filterExpression;
+
+                    return node;
+                }
                 default:
-                    throw Utils.NotSupportedExpression(lambdaExpression.Body);
+                    throw Utils.NotSupportedExpression(node);
             }
         }
 
-        private static IEnumerable<ODataExpandAssociation> ExtractExpandAssociations(Expression expression, ITypeCache typeCache)
+        protected override Expression VisitNew(NewExpression node)
         {
-            switch (expression.NodeType)
-            {
-                case ExpressionType.MemberAccess:
-                    return ExtractExpandAssociations(expression as MemberExpression, typeCache);
-
-                case ExpressionType.Convert:
-                    return ExtractExpandAssociations((expression as UnaryExpression).Operand, typeCache);
-
-                case ExpressionType.Lambda:
-                    return ExtractExpandAssociations((expression as LambdaExpression).Body, typeCache);
-
-                case ExpressionType.Call:
-                    return ExtractExpandAssociations(expression as MethodCallExpression, typeCache);
-
-                case ExpressionType.New:
-                    return ExtractExpandAssociations(expression as NewExpression, typeCache);
-
-                default:
-                    throw Utils.NotSupportedExpression(expression);
-            }
+            ExpandAssociations.AddRange(node.Arguments.SelectMany(ExtractNestedExpandAssociations));
+            return node;
         }
 
-        private static IEnumerable<ODataExpandAssociation> ExtractExpandAssociations(MethodCallExpression callExpression, ITypeCache typeCache)
+        private IEnumerable<ODataExpandAssociation> ExtractNestedExpandAssociations(Expression expression)
         {
-            var associations = ExtractExpandAssociations(callExpression.Arguments[0], typeCache);
-            if (callExpression.Method.Name == "Select" && callExpression.Arguments.Count == 2)
-            {
-                associations.Last().ExpandAssociations
-                    .AddRange(ExtractExpandAssociations(callExpression.Arguments[1], typeCache));
-
-                return associations;
-            }
-
-            if ((callExpression.Method.Name == "OrderBy" ||
-                 callExpression.Method.Name == "ThenBy") && callExpression.Arguments.Count == 2)
-            {
-                if (callExpression.Arguments[0] is MethodCallExpression &&
-                    ((callExpression.Arguments[0] as MethodCallExpression).Method.Name == "Select"))
-                    throw Utils.NotSupportedExpression(callExpression);
-
-                associations.Last().OrderByColumns
-                    .AddRange(callExpression.Arguments[1]
-                        .ExtractColumnNames(typeCache).Select(c => new ODataOrderByColumn(c, false)));
-
-                return associations;
-            }
-
-            if ((callExpression.Method.Name == "OrderByDescending" ||
-                 callExpression.Method.Name == "ThenByDescending") && callExpression.Arguments.Count == 2)
-            {
-                if (callExpression.Arguments[0] is MethodCallExpression &&
-                    ((callExpression.Arguments[0] as MethodCallExpression).Method.Name == "Select"))
-                    throw Utils.NotSupportedExpression(callExpression);
-
-                associations.Last().OrderByColumns
-                    .AddRange(callExpression.Arguments[1]
-                        .ExtractColumnNames(typeCache).Select(c => new ODataOrderByColumn(c, true)));
-
-                return associations;
-            }
-
-            if (callExpression.Method.Name == "Where" && callExpression.Arguments.Count == 2)
-            {
-                var filterExpression =
-                    ODataExpression.FromLinqExpression((callExpression.Arguments[1] as LambdaExpression).Body);
-                var association = associations.Last();
-                if (ReferenceEquals(association.FilterExpression, null))
-                    association.FilterExpression = filterExpression;
-                else
-                    association.FilterExpression = association.FilterExpression && filterExpression;
-                return associations;
-            }
-
-            throw Utils.NotSupportedExpression(callExpression);
-        }
-
-        private static IEnumerable<ODataExpandAssociation> ExtractExpandAssociations(NewExpression newExpression, ITypeCache typeCache)
-        {
-            return newExpression.Arguments.SelectMany(x => ExtractExpandAssociations(x, typeCache));
-        }
-
-        private static IEnumerable<ODataExpandAssociation> ExtractExpandAssociations(MemberExpression memberExpression, ITypeCache typeCache)
-        {
-            var memberName = typeCache.GetMappedName(memberExpression.Expression.Type, memberExpression.Member.Name);
-
-            return memberExpression.Expression is MemberExpression
-                ? ExtractExpandAssociations(memberExpression.Expression, typeCache)
-                : new[] { new ODataExpandAssociation(memberName) };
+            var nestedExpandExpressionVisitor = new ExpandExpressionVisitor(_typeCache);
+            nestedExpandExpressionVisitor.Visit(expression);
+            return nestedExpandExpressionVisitor.ExpandAssociations;
         }
     }
 }
