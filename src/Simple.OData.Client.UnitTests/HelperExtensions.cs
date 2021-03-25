8466758064617afd201dc4e83714de836b3b9c4f
Νίκος Σαρρής <nsarris@vivawallet.com>
2021-03-25 16:29:24 +0200

    Fixed bug with selected property missing when its named is contained in an expanded property.

---
+++ src/Simple.OData.Client.UnitTests/HelperExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.OData.Client.Tests
+{
+    internal static class HelperExtensions
+    {
+        /// <summary>
+        /// Helper extension to derive the collection type from an instance type. Use to work with anonymous types as entity classes in tests. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="oDataClient"></param>
+        /// <param name="_"></param>
+        /// <param name="collectionName"></param>
+        /// <returns></returns>
+        public static IBoundClient<T> For<T>(this IODataClient oDataClient, T _, string collectionName)
+            where T : class
+           => oDataClient.For<T>(collectionName);
+    }
+}
