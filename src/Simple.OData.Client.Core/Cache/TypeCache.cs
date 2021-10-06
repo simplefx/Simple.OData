ce6c73ab317ab105a33d21abc2f9ec81f68df4ff
Vagif Abilov <vagif.abilov@gmail.com>
2021-10-06 10:21:36 +0200

    Added missing namespace.

--- src/Simple.OData.Client.Core/Cache/TypeCache.cs
+++ src/Simple.OData.Client.Core/Cache/TypeCache.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
 
 namespace Simple.OData.Client
