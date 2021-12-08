e7e237b673852d8bad2705502d33e4e88684ecf4
David Bond <david.bond@panoramicdata.com>
2021-12-08 23:07:44 +0000

    Fixed MoviesContext issue

--- src/WebApiOData.V4.Samples/Controllers/MoviesController.cs
+++ src/WebApiOData.V4.Samples/Controllers/MoviesController.cs
@@ -8,111 +8,111 @@ using WebApiOData.V4.Samples.Models;
 
 namespace WebApiOData.V4.Samples.Controllers
 {
-    public class MoviesController : ODataController
-    {
-        private readonly MoviesContext _db = new MoviesContext();
+	public class MoviesController : ODataController
+	{
+		private readonly MoviesContext _db = new MoviesContext();
 
-        public IHttpActionResult Get()
-        {
-            return Ok(MoviesContext.Movies);
-        }
+		public IHttpActionResult Get()
+		{
+			return Ok(_db.Movies);
+		}
 
-        [HttpPost]
-        public IHttpActionResult CheckOut(int key)
-        {
-            var movie = MoviesContext.Movies.FirstOrDefault(m => m.ID == key);
-            if (movie == null)
-            {
-                return BadRequest(ModelState);
-            }
+		[HttpPost]
+		public IHttpActionResult CheckOut(int key)
+		{
+			var movie = _db.Movies.FirstOrDefault(m => m.ID == key);
+			if (movie == null)
+			{
+				return BadRequest(ModelState);
+			}
 
-            if (!TryCheckoutMovie(movie))
-            {
-                return BadRequest("The movie is already checked out.");
-            }
+			if (!TryCheckoutMovie(movie))
+			{
+				return BadRequest("The movie is already checked out.");
+			}
 
-            return Ok(movie);
-        }
+			return Ok(movie);
+		}
 
-        [HttpPost]
-        public IHttpActionResult Return(int key)
-        {
-            var movie = MoviesContext.Movies.FirstOrDefault(m => m.ID == key);
-            if (movie == null)
-            {
-                return BadRequest(ModelState);
-            }
+		[HttpPost]
+		public IHttpActionResult Return(int key)
+		{
+			var movie = _db.Movies.FirstOrDefault(m => m.ID == key);
+			if (movie == null)
+			{
+				return BadRequest(ModelState);
+			}
 
-            movie.DueDate = null;
+			movie.DueDate = null;
 
-            return Ok(movie);
-        }
+			return Ok(movie);
+		}
 
-        // Check out a list of movies.
-        [HttpPost]
-        public IHttpActionResult CheckOutMany(ODataActionParameters parameters)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+		// Check out a list of movies.
+		[HttpPost]
+		public IHttpActionResult CheckOutMany(ODataActionParameters parameters)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
 
-            // Client passes a list of movie IDs to check out.
-            var movieIDs = new HashSet<int>(parameters["MovieIDs"] as IEnumerable<int>);
+			// Client passes a list of movie IDs to check out.
+			var movieIDs = new HashSet<int>(parameters["MovieIDs"] as IEnumerable<int>);
 
-            // Try to check out each movie in the list.
-            var results = new List<Movie>();
-            foreach (var movie in MoviesContext.Movies.Where(m => movieIDs.Contains(m.ID)))
-            {
-                if (TryCheckoutMovie(movie))
-                {
-                    results.Add(movie);
-                }
-            }
+			// Try to check out each movie in the list.
+			var results = new List<Movie>();
+			foreach (var movie in _db.Movies.Where(m => movieIDs.Contains(m.ID)))
+			{
+				if (TryCheckoutMovie(movie))
+				{
+					results.Add(movie);
+				}
+			}
 
-            // Return a list of the movies that were checked out.
-            return Ok(results);
-        }
+			// Return a list of the movies that were checked out.
+			return Ok(results);
+		}
 
-        [HttpPost]
-        [ODataRoute("CreateMovie")]
-        public IHttpActionResult CreateMovie(ODataActionParameters parameters)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+		[HttpPost]
+		[ODataRoute("CreateMovie")]
+		public IHttpActionResult CreateMovie(ODataActionParameters parameters)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
 
-            var title = parameters["Title"] as string;
+			var title = parameters["Title"] as string;
 
-            var movie = new Movie()
-            {
-                Title = title,
-                ID = MoviesContext.Movies.Count + 1,
-            };
+			var movie = new Movie()
+			{
+				Title = title,
+				ID = _db.Movies.Count + 1,
+			};
 
-			MoviesContext.Movies.Add(movie);
+			_db.Movies.Add(movie);
 
-            return Created(movie);
-        }
+			return Created(movie);
+		}
 
-        protected Movie GetMovieByKey(int key)
-        {
-            return MoviesContext.Movies.FirstOrDefault(m => m.ID == key);
-        }
+		protected Movie GetMovieByKey(int key)
+		{
+			return _db.Movies.FirstOrDefault(m => m.ID == key);
+		}
 
-        private static bool TryCheckoutMovie(Movie movie)
-        {
-            if (movie.IsCheckedOut)
-            {
-                return false;
-            }
-            else
-            {
-                // To check out a movie, set the due date.
-                movie.DueDate = DateTime.Now.AddDays(7);
-                return true;
-            }
-        }
-    }
+		private static bool TryCheckoutMovie(Movie movie)
+		{
+			if (movie.IsCheckedOut)
+			{
+				return false;
+			}
+			else
+			{
+				// To check out a movie, set the due date.
+				movie.DueDate = DateTime.Now.AddDays(7);
+				return true;
+			}
+		}
+	}
 }
--- src/WebApiOData.V4.Samples/Controllers/ProductsController.cs
+++ src/WebApiOData.V4.Samples/Controllers/ProductsController.cs
@@ -9,161 +9,161 @@ using WebApiOData.V4.Samples.Models;
 
 namespace WebApiOData.V4.Samples.Controllers
 {
-    public class ProductsController : ODataController
-    {
-        private static readonly ConcurrentDictionary<int, Product> _data;
+	public class ProductsController : ODataController
+	{
+		private static readonly ConcurrentDictionary<int, Product> _data;
 
-        static ProductsController()
-        {
-            _data = new ConcurrentDictionary<int, Product>();
-            var rand = new Random();
+		static ProductsController()
+		{
+			_data = new ConcurrentDictionary<int, Product>();
+			var rand = new Random();
 
-            Enumerable.Range(0, 100)
-                .Select(i => new Product
-                {
-                    Id = i,
-                    Name = "Product " + i,
-                    Price = rand.NextDouble() * 1000
-                })
-                .ToList()
-                .ForEach(p => _data.TryAdd(p.Id, p));
-        }
+			Enumerable.Range(0, 100)
+				.Select(i => new Product
+				{
+					Id = i,
+					Name = "Product " + i,
+					Price = rand.NextDouble() * 1000
+				})
+				.ToList()
+				.ForEach(p => _data.TryAdd(p.Id, p));
+		}
 
-        public ProductsController()
-        {
-        }
+		public ProductsController()
+		{
+		}
 
-        [Route("*")]
-        public IHttpActionResult Default()
-        {
-            return Ok("OK!!!");
-        }
+		[Route("*")]
+		public IHttpActionResult Default()
+		{
+			return Ok("OK!!!");
+		}
 
-        [EnableQuery]
-        public static IQueryable<Product> Get()
-        {
-            return _data.Values.AsQueryable();
-        }
+		[EnableQuery]
+		public static IQueryable<Product> Get()
+		{
+			return _data.Values.AsQueryable();
+		}
 
-        public IHttpActionResult GetProduct(int key)
-        {
-            if (_data.TryGetValue(key, out var retval))
-            {
-                return Ok(retval);
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
+		public IHttpActionResult GetProduct(int key)
+		{
+			if (_data.TryGetValue(key, out var retval))
+			{
+				return Ok(retval);
+			}
+			else
+			{
+				return NotFound();
+			}
+		}
 
-        [HttpGet]
-        public IHttpActionResult MostExpensive()
-        {
-            var retval = _data.Max(pair => pair.Value.Price);
+		[HttpGet]
+		public IHttpActionResult MostExpensive()
+		{
+			var retval = _data.Max(pair => pair.Value.Price);
 
-            return Ok(retval);
-        }
+			return Ok(retval);
+		}
 
-        // Returns top 3 most expensive products
-        // This is needed to check function name matching
-        [HttpGet]
-        public IHttpActionResult MostExpensives()
-        {
-            var retval = _data.Values.OrderByDescending(p => p.Price).Take(3).ToList();
+		// Returns top 3 most expensive products
+		// This is needed to check function name matching
+		[HttpGet]
+		public IHttpActionResult MostExpensives()
+		{
+			var retval = _data.Values.OrderByDescending(p => p.Price).Take(3).ToList();
 
-            return Ok(retval);
-        }
+			return Ok(retval);
+		}
 
-        // Returns the top ten most expensive products
-        [HttpGet]
-        public IHttpActionResult Top10()
-        {
-            var retval = _data.Values.OrderByDescending(p => p.Price).Take(10).ToList();
+		// Returns the top ten most expensive products
+		[HttpGet]
+		public IHttpActionResult Top10()
+		{
+			var retval = _data.Values.OrderByDescending(p => p.Price).Take(10).ToList();
 
-            return Ok(retval);
-        }
+			return Ok(retval);
+		}
 
-        [HttpGet]
-        public IHttpActionResult GetPriceRank(int key)
-        {
-            if (_data.TryGetValue(key, out var product))
-            {
-                // NOTE: Use where clause to get the rank of the price may not
-                // offer the good time complexity. The following code is intended
-                // for demostration only.
-                return Ok(_data.Values.Count(one => one.Price > product.Price));
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
+		[HttpGet]
+		public IHttpActionResult GetPriceRank(int key)
+		{
+			if (_data.TryGetValue(key, out var product))
+			{
+				// NOTE: Use where clause to get the rank of the price may not
+				// offer the good time complexity. The following code is intended
+				// for demostration only.
+				return Ok(_data.Values.Count(one => one.Price > product.Price));
+			}
+			else
+			{
+				return NotFound();
+			}
+		}
 
-        [HttpGet]
-        public IHttpActionResult CalculateGeneralSalesTax(int key, string state)
-        {
-            var taxRate = GetRate(state);
+		[HttpGet]
+		public IHttpActionResult CalculateGeneralSalesTax(int key, string state)
+		{
+			var taxRate = GetRate(state);
 
-            if (_data.TryGetValue(key, out var product))
-            {
-                var tax = product.Price * taxRate / 100;
-                return Ok(tax);
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
+			if (_data.TryGetValue(key, out var product))
+			{
+				var tax = product.Price * taxRate / 100;
+				return Ok(tax);
+			}
+			else
+			{
+				return NotFound();
+			}
+		}
 
-        [HttpGet]
-        [ODataRoute("GetSalesTaxRate(state={state})")]
-        public IHttpActionResult GetSalesTaxRate([FromODataUri] string state)
-        {
-            return Ok(GetRate(state));
-        }
-        
-        [HttpGet]
-        [EnableQuery]
-        [ODataRoute("Products({key})/Default.Placements()")]
-        public IHttpActionResult Placements([FromODataUri] int key, ODataQueryOptions<Movie> options)
-        {
-            var source = new MoviesContext().Movies.Where(x=>x.ID < key).AsQueryable();
-            return Ok(source);
-        }
+		[HttpGet]
+		[ODataRoute("GetSalesTaxRate(state={state})")]
+		public IHttpActionResult GetSalesTaxRate([FromODataUri] string state)
+		{
+			return Ok(GetRate(state));
+		}
 
-        private static double GetRate(string state)
-        {
-            double taxRate = 0;
-            switch (state)
-            {
-                case "AZ": taxRate = 5.6; break;
-                case "CA": taxRate = 7.5; break;
-                case "CT": taxRate = 6.35; break;
-                case "GA": taxRate = 4; break;
-                case "IN": taxRate = 7; break;
-                case "KS": taxRate = 6.15; break;
-                case "KY": taxRate = 6; break;
-                case "MA": taxRate = 6.25; break;
-                case "NV": taxRate = 6.85; break;
-                case "NJ": taxRate = 7; break;
-                case "NY": taxRate = 4; break;
-                case "NC": taxRate = 4.75; break;
-                case "ND": taxRate = 5; break;
-                case "PA": taxRate = 6; break;
-                case "TN": taxRate = 7; break;
-                case "TX": taxRate = 6.25; break;
-                case "VA": taxRate = 4.3; break;
-                case "WA": taxRate = 6.5; break;
-                case "WV": taxRate = 6; break;
-                case "WI": taxRate = 5; break;
+		[HttpGet]
+		[EnableQuery]
+		[ODataRoute("Products({key})/Default.Placements()")]
+		public IHttpActionResult Placements([FromODataUri] int key, ODataQueryOptions<Movie> options)
+		{
+			var source = new MoviesContext().Movies.Where(x => x.ID < key).AsQueryable();
+			return Ok(source);
+		}
 
-                default:
-                    taxRate = 0;
-                    break;
-            }
+		private static double GetRate(string state)
+		{
+			double taxRate = 0;
+			switch (state)
+			{
+				case "AZ": taxRate = 5.6; break;
+				case "CA": taxRate = 7.5; break;
+				case "CT": taxRate = 6.35; break;
+				case "GA": taxRate = 4; break;
+				case "IN": taxRate = 7; break;
+				case "KS": taxRate = 6.15; break;
+				case "KY": taxRate = 6; break;
+				case "MA": taxRate = 6.25; break;
+				case "NV": taxRate = 6.85; break;
+				case "NJ": taxRate = 7; break;
+				case "NY": taxRate = 4; break;
+				case "NC": taxRate = 4.75; break;
+				case "ND": taxRate = 5; break;
+				case "PA": taxRate = 6; break;
+				case "TN": taxRate = 7; break;
+				case "TX": taxRate = 6.25; break;
+				case "VA": taxRate = 4.3; break;
+				case "WA": taxRate = 6.5; break;
+				case "WV": taxRate = 6; break;
+				case "WI": taxRate = 5; break;
 
-            return taxRate;
-        }
-    }
+				default:
+					taxRate = 0;
+					break;
+			}
+
+			return taxRate;
+		}
+	}
 }
